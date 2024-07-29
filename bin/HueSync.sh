9d2f4139d1111bccca196471782559579eeabb50
fewtarius <fewtarius@steamfork.org>
2024-07-29 19:31:29 +0000

    Systemd should NOT wait 45 seconds to timeout a sighup of the decky loader plugin service...

--- bin/HueSync.sh
+++ bin/HueSync.sh
@@ -3,5 +3,7 @@ if [ ! -f "${HOME}/homebrew/services/PluginLoader" ]
 then
         echo "Installing: Decky Loader (Prerequisite)"
         curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+	sudo sed -i 's~TimeoutStopSec=.*$~TimeoutStopSec=2~g' /etc/systemd/system/plugin_loader.service
+	sudo systemctl daemon-reload
 fi
 curl -L https://raw.githubusercontent.com/honjow/huesync/main/install.sh | sh
--- bin/SimpleDeckyTDP.sh
+++ bin/SimpleDeckyTDP.sh
@@ -3,5 +3,7 @@ if [ ! -f "${HOME}/homebrew/services/PluginLoader" ]
 then
         echo "Installing: Decky Loader (Prerequisite)"
         curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+        sudo sed -i 's~TimeoutStopSec=.*$~TimeoutStopSec=2~g' /etc/systemd/system/plugin_loader.service
+        sudo systemctl daemon-reload
 fi
 curl -L https://github.com/SteamFork/SimpleDeckyTDP/raw/main/install.sh | sh
