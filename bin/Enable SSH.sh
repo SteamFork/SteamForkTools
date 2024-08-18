c8a96445754b24bf5c68fc1adeef7e555b8be82d
fewtarius <fewtarius@steamfork.org>
2024-08-18 12:34:19 +0000

    Don't need to enable root RW for ssh.

--- bin/Enable SSH.sh
+++ bin/Enable SSH.sh
@@ -2,7 +2,5 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2024 SteamFork (https://github.com/SteamFork)
 
-sudo steamos-readonly disable
 sudo systemctl enable sshd
 sudo systemctl start sshd
-sudo steamos-readonly enable
