c3f544ef1cafdd4b9a6f39a8025eebbc3188d921
fewtarius <fewtarius@steamfork.org>
2024-08-30 11:11:29 +0000

    Deprecate controller quirks, add DeckyPlumber for per game input control.  Thanks to @aarron-lee!

---
+++ bin/DeckyPlumber.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024 SteamFork (https://github.com/SteamFork)
+
+if [ ! -f "${HOME}/homebrew/services/PluginLoader" ]
+then
+		DECKY="FALSE"
+else
+		DECKY="TRUE"
+fi
+
+if [ ! -f "${HOME}/homebrew/plugins/SimpleDeckyTDP/package.json" ]
+then
+	INSTALLED="FALSE"
+else
+	INSTALLED="TRUE"
+fi
+
+case ${1} in
+	check)
+		echo "${INSTALLED}"
+		exit 0
+		;;
+	TRUE)
+		if [ "${INSTALLED}" = "TRUE" ]
+		then
+			echo "Already installed."
+			exit 0
+		fi
+		if [ "${DECKY}" = "FALSE" ]
+		then
+			${SCRIPT_PATH}/"Decky Loader.sh"
+		fi
+		curl -L https://github.com/aarron-lee/DeckyPlumber/raw/main/install.sh | sh
+		;;
+	FALSE)
+
+		if [ "${INSTALLED}" = "FALSE" ]
+		then
+			echo "Nothing to do."
+			exit 0
+		fi
+		sudo systemctl stop plugin_loader.service
+		sudo rm -rf ${HOME}/homebrew/plugins/DeckyPlumber
+		sudo systemctl start plugin_loader.service
+		;;
+esac
