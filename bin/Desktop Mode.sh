6bc1e14421ec2715add2b2c4dc440cd6efcd8d3c
fewtarius <fewtarius@steamfork.org>
2025-04-12 09:23:30 +0000

    Update Desktop Mode switch to work with 3.7.

--- bin/Desktop Mode.sh
+++ bin/Desktop Mode.sh
@@ -4,7 +4,7 @@
 
 source steamfork-devicequirk-set
 
-if [ -f "/etc/X11/xorg.conf.d/99-touchscreen_orientation.conf" ]
+if [ -f "/etc/steamfork-default-session" ]
 then
 	ENABLED="TRUE"
 else
@@ -22,65 +22,10 @@ case ${1} in
 			echo "Already installed."
 			exit 0
 		fi
-		sudo steamos-readonly disable
-		sudo steamfork-disable-sessions
-		cat <<EOF | sudo tee /etc/sddm.conf.d/001-rotation.conf
-[XDisplay]
-DisplayCommand=/etc/X11/Xsession.d/999rotate-screen
-EOF
-
-			case ${X11_ROTATION} in
-				left)
-					X11_TOUCH="0 -1 1 1 0 0 0 0 1"
-					;;
-				right)
-					X11_TOUCH="0 1 0 -1 0 1 0 0 1"
-					;;
-				normal)
-					X11_TOUCH="1 0 0 0 1 0 0 0 1"
-					;;
-				inverted)
-					X11_TOUCH="-1 0 1 0 -1 1 0 0 1"
-					;;
-			esac
-
-		cat <<EOF | sudo tee /etc/X11/xorg.conf.d/99-touchscreen_orientation.conf
-Section "InputClass"
-	Identifier "Coordinate Transformation Matrix"
-	MatchIsTouchscreen "on"
-	MatchDevicePath "/dev/input/event*"
-	MatchDriver "libinput"
-	Option "CalibrationMatrix" "${X11_TOUCH}"
-EndSection
-EOF
-			sudo steamos-readonly enable
-			;;
-		FALSE)
-			if [ "${ENABLED}" = "FALSE" ]
-			then
-				echo "Nothing to do."
-				exit 0
-			fi
-			source steamfork-devicequirk-set
-			sudo steamos-readonly disable
-			sudo steamfork-enable-sessions
-			sudo rm -f /etc/sddm.conf.d/001-rotation.conf 2>/dev/null
-			sudo rm -f /etc/X11/xorg.conf.d/99-touchscreen_orientation.conf 2>/dev/null
-
-			cat <<EOF | sudo tee /etc/sddm.conf.d/steamos.conf
-[General]
-DisplayServer=wayland
-
-[Autologin]
-User=deck
-Session=gamescope-wayland.desktop
-Relogin=true
-
-[X11]
-# Janky workaround for wayland sessions not stopping in sddm, kills
-# all active sddm-helper sessions on teardown
-DisplayStopCommand=/usr/bin/gamescope-wayland-teardown-workaround
-EOF
-		sudo steamos-readonly enable
+    steamos-session-select plasma-wayland-persistent
+    echo plasma-wayland-persistent | sudo tee /etc/steamfork-default-session > /dev/null
 		;;
+  FALSE)
+    steamos-session-select gamescope
+    ;;
 esac
