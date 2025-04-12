cfba9f5a37c2d6b38237b3810805f538ab6e3d28
fewtarius <fewtarius@steamfork.org>
2025-04-12 09:58:44 +0000

    Misc script cleanup.

--- bin/SimpleDeckyTDP.sh
+++ bin/SimpleDeckyTDP.sh
@@ -4,46 +4,42 @@
 
 SCRIPT_PATH="${SCRIPT_PATH:-$(dirname $(realpath "$0"))}"
 
-if [ ! -f "${HOME}/homebrew/services/PluginLoader" ]
-then
-		DECKY="FALSE"
+if [ ! -f "${HOME}/homebrew/services/PluginLoader" ]; then
+    DECKY="FALSE"
 else
-		DECKY="TRUE"
+    DECKY="TRUE"
 fi
 
-if [ ! -f "${HOME}/homebrew/plugins/SimpleDeckyTDP/package.json" ]
-then
-	INSTALLED="FALSE"
+if [ ! -f "${HOME}/homebrew/plugins/SimpleDeckyTDP/package.json" ]; then
+    INSTALLED="FALSE"
 else
-	INSTALLED="TRUE"
+    INSTALLED="TRUE"
 fi
 
 case ${1} in
-	check)
-		echo "${INSTALLED}"
-		exit 0
-		;;
-	TRUE)
-		if [ "${INSTALLED}" = "TRUE" ]
-		then
-			echo "Already installed."
-			exit 0
-		fi
-		if [ "${DECKY}" = "FALSE" ]
-		then
-			${SCRIPT_PATH}/"Decky Loader.sh"
-		fi
-		curl -L https://github.com/aarron-lee/SimpleDeckyTDP/raw/main/install.sh | sh
-		;;
-	FALSE)
-
-		if [ "${INSTALLED}" = "FALSE" ]
-		then
-			echo "Nothing to do."
-			exit 0
-		fi
-		sudo systemctl stop plugin_loader.service
-		sudo rm -rf ${HOME}/homebrew/plugins/SimpleDeckyTDP
-		sudo systemctl start plugin_loader.service
-		;;
+    check)
+        echo "${INSTALLED}"
+        exit 0
+        ;;
+    TRUE)
+        if [ "${INSTALLED}" = "TRUE" ]; then
+            echo "SimpleDeckyTDP is already installed."
+            exit 0
+        fi
+        if [ "${DECKY}" = "FALSE" ]; then
+            "${SCRIPT_PATH}/Decky Loader.sh" TRUE
+        fi
+        echo "Installing SimpleDeckyTDP..."
+        curl -L https://github.com/aarron-lee/SimpleDeckyTDP/raw/main/install.sh | sh
+        ;;
+    FALSE)
+        if [ "${INSTALLED}" = "FALSE" ]; then
+            echo "SimpleDeckyTDP is not installed."
+            exit 0
+        fi
+        echo "Uninstalling SimpleDeckyTDP..."
+        sudo systemctl stop plugin_loader.service
+        sudo rm -rf "${HOME}/homebrew/plugins/SimpleDeckyTDP"
+        sudo systemctl start plugin_loader.service
+        ;;
 esac
